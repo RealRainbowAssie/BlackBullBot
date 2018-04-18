448a9041d606ff8c5fc60d1f65ff645406b1654c
RealRainbowAssie <38475910+RealRainbowAssie@users.noreply.github.com>
2018-04-18 17:24:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ bot.on("message", function(message) {
             message.channel.sendMessage("Pong!");
             break;
         case "info":
-            message.channel.sendMessage("Ik ben BlackBull, Waar mijn naam vandaan komt? Asmar was een keer RedBull aan het drinken, En had snel een naam nodig ENNN hij had gewoon zin om een bot te maken i think.");
+            message.channel.sendMessage("Ik ben BlackBull, Waar mijn naam vandaan komt, Asmar was een keer RedBull aan het drinken, En had snel een naam nodig ENNN hij had gewoon zin om een bot te maken i think.");
             break;
         case "8ball":
         if (args [1]) message.channel.sendMessage(fortunes[Math.floor(Math.random() * fortunes.length)]);
