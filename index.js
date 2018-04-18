7bf579d7a64e8940c32c1b98e6ef20742dbec703
RealRainbowAssie <38475910+RealRainbowAssie@users.noreply.github.com>
2018-04-18 16:35:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { arch } from "os";
+
 const Discord = require("discord.js");
 
 const TOKEN = "NDM1ODM5MjUxMDM3NTUyNjcx.Dbe0lA.81mjftpjYlZvHgf8u5Z3Cyz-9Ew"; 
@@ -36,6 +38,35 @@ bot.on("message", function(message) {
 
     var args = message.content.substring(PREFIX.length).split(" ");
 
+    if(cmd == `${prefix}kick`){
+      
+       let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+       if(!kUser) return message.channel.send("Kan persoon niet vinden");
+       let kReason = args.join(" ").slice(22);
+       if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Je hebt hier geen permissie voor!")
+       if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Diegene kan niet gekicked worden!")
+
+
+       let kickEmbed = new Discord.RichEmbed()
+       .setDescription("~Kick~")
+       .setColor("#E81F3F")
+       .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
+       .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
+       .addField("Kicked In", message.channel)
+       .addField("Tiime", message.createdAt)
+       .addField("Reason", kReason);
+
+       let kickChannel = Message.guild.channels.find(`name`, "warn-system");
+       if(!kickChannel) return message.channel.send("Kan warn-system channel niet vinden.");
+
+       message.guild.member(kUser).kick(kReason);
+       kickChannel.send(kickEmbed)
+
+        return;
+    }
+
+
+
     switch (args[0].toLocaleLowerCase()) {
        case "ping":
             message.channel.sendMessage("Pong!");
@@ -59,32 +90,15 @@ bot.on("message", function(message) {
                 .setThumbnail(message.author.avatarURL)  
             message.channel.sendEmbed(embed);
             break;
-    module.exports.run = async (bot, message, args) => {
-    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
-    let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
-    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
-
-    let kickEmbed = new Discord.RichEmbed()
-    .setDescription("~Kick~")
-    .setColor("#e56b00")
-    .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-    .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-    .addField("Kicked In", message.channel)
-    .addField("Tiime", message.createdAt)
-    .addField("Reason", kReason);
-
-    let kickChannel = message.guild.channels.find(`name`, "incidents");
-    if(!kickChannel) return message.channel.send("Can't find incidents channel.");
-
-    message.guild.member(kUser).kick(kReason);
-    kickChannel.send(kickEmbed);
-}
-
-module.exports.help = {
-  name:"kick"
-}
+    
+        case "commands":
+            var embed = new Discord.RichEmbed()
+                .addField("Commands", "!ping , !info , !8ball , !twitch , !commands", true)
+                .setColor(0xf1be4)
+                .setFooter("Binnenkort meer informatie hier over!") 
+                .setThumbnail(message.author.avatarURL)  
+            message.channel.sendEmbed(embed);
+            break;
     
             
     }
