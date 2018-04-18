3773824e623802b4c82d4af605bbf8749b2ea0d7
RealRainbowAssie <38475910+RealRainbowAssie@users.noreply.github.com>
2018-04-18 16:42:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -71,32 +71,7 @@ bot.on("message", function(message) {
             message.channel.sendEmbed(embed);
             break;
 
-            if(cmd == `${prefix}kick`){
-      
-                let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-                if(!kUser) return message.channel.send("Kan persoon niet vinden");
-                let kReason = args.join(" ").slice(22);
-                if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Je hebt hier geen permissie voor!")
-                if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Diegene kan niet gekicked worden!")
-         
-         
-                let kickEmbed = new Discord.RichEmbed()
-                .setDescription("~Kick~")
-                .setColor("#E81F3F")
-                .addField("Kicked User", `${kUser} with ID ${kUser.id}`)
-                .addField("Kicked By", `<@${message.author.id}> with ID ${message.author.id}`)
-                .addField("Kicked In", message.channel)
-                .addField("Tiime", message.createdAt)
-                .addField("Reason", kReason);
-         
-                let kickChannel = Message.guild.channels.find(`name`, "warn-system");
-                if(!kickChannel) return message.channel.send("Kan warn-system channel niet vinden.");
-         
-                message.guild.member(kUser).kick(kReason);
-                kickChannel.send(kickEmbed)
-         
-                 return;
-             }        
+               
     
             
     }
